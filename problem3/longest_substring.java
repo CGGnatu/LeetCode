5c19550bc409e4e720598da4cee79ab38129a491
CGGnatu <32790933+CGGnatu@users.noreply.github.com>
2018-03-31 22:19:02 +0800

    bug exist!
    for String like pwwkew, result is unexpected

--- problem3/longest_substring.java
+++ problem3/longest_substring.java
@@ -2,7 +2,7 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int len=0,result=0;
         for(int j=0;j<s.length();j++){
-            String substr="";
+            String substr="";  //reset substr in every out loop
             for(int i=j;i<s.length();i++){
                 if(!substr.contains(String.valueOf(s.charAt(i)))){
                    substr=substr+s.charAt(i);
